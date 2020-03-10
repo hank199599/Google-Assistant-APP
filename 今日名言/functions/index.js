46ae87e1063e0e858012221c8bb37cc11f167dd5
hank199599 <36280707+hank199599@users.noreply.github.com>
2020-03-10 16:49:44 +0800

    Update index.js

--- 今日名言/functions/index.js
+++ 今日名言/functions/index.js
@@ -25,7 +25,7 @@ var picker = require('./daily_quotes');
 var output_array="";
 var Quote="";
 var reference="";
-var Picture_url="";
+var Picture_url="";var date="";
 var theArray= new Array;
 theArray=["https://i.imgur.com/02rnSIa.jpg","https://i.imgur.com/VfcMtId.jpg","https://i.imgur.com/PLVkbbK.jpg","https://i.imgur.com/LcEpziD.jpg","https://i.imgur.com/EoCooEB.jpg","https://i.imgur.com/hfLI7eK.jpg","https://i.imgur.com/71beT0V.jpg","https://i.imgur.com/FcfNl1w.jpg","https://i.imgur.com/APcShEp.jpg","https://i.imgur.com/4R8PxKA.jpg","https://i.imgur.com/b5vLpDA.jpg","https://i.imgur.com/faaRLCP.jpg","https://i.imgur.com/pTci1ux.jpg","https://i.imgur.com/lYmqldk.jpg","https://i.imgur.com/2oOhmvs.jpg","https://i.imgur.com/2oOhmvs.jpg"];
 
@@ -52,7 +52,7 @@ app.intent('預設歡迎語句', (conv) => {
   
 		conv.ask(new SimpleResponse({               
 			speech: `<speak><p><s>以下是${date}的名言</s><break time="0.25s"/><s>${Quote}</s><break time="0.5s"/><s>出自<break time="0.3s"/>${reference}</s></p></speak>`,
-			text: '以下是今日'+date+'的名言：',}));
+			text: '以下是今日'+date+'的名言',}));
 
         conv.close(new BasicCard({   
 				image: new Image({url:Picture_url,alt:'Pictures',}),

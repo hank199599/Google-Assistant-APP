584b59e0f991d127c1b79325a6380abc0577d13f
hank199599 <36280707+hank199599@users.noreply.github.com>
2020-03-10 16:47:29 +0800

    Update index.js

--- 今日名言/functions/index.js
+++ 今日名言/functions/index.js
@@ -22,41 +22,28 @@ const functions = require('firebase-functions');
 const replaceString = require('replace-string');
 const app = dialogflow({debug: true});
 var picker = require('./daily_quotes');
-var Time= new Date();
-var date="";var month="";var day="";
 var output_array="";
 var Quote="";
 var reference="";
 var Picture_url="";
-
-var months = "1月,2月,3月,4月,5月,6月,7月,8月,9月,10月,11月,12月".split(",");
-var month =String(months[Time.getMonth()]);
-var day =String(Time.getDate());
-var Hournow=Time.getHours()+8;
 var theArray= new Array;
 theArray=["https://i.imgur.com/02rnSIa.jpg","https://i.imgur.com/VfcMtId.jpg","https://i.imgur.com/PLVkbbK.jpg","https://i.imgur.com/LcEpziD.jpg","https://i.imgur.com/EoCooEB.jpg","https://i.imgur.com/hfLI7eK.jpg","https://i.imgur.com/71beT0V.jpg","https://i.imgur.com/FcfNl1w.jpg","https://i.imgur.com/APcShEp.jpg","https://i.imgur.com/4R8PxKA.jpg","https://i.imgur.com/b5vLpDA.jpg","https://i.imgur.com/faaRLCP.jpg","https://i.imgur.com/pTci1ux.jpg","https://i.imgur.com/lYmqldk.jpg","https://i.imgur.com/2oOhmvs.jpg","https://i.imgur.com/2oOhmvs.jpg"];
 
-app.intent('預設歡迎語句', (conv) => { 
-	Time= new Date();
-	month =months[Time.getMonth()];
-	day =Time.getDate();
-	Hournow=Time.getHours()+8;
-	if(Hournow>=24){day++;}
-	date=month+day+'日';
-    if(date==="1月32日"){date="2月1日"}
-	else if(date==="2月29日"&&(Time.getFullYear()%4)!==0){date="3月1日"}
-	else if(date==="2月30日"&&(Time.getFullYear()%4)===0){date="3月1日"}
-    else if(date==="3月32日"){date="4月1日"}
-    else if(date==="4月31日"){date="5月1日"}
-    else if(date==="5月32日"){date="6月1日"}
-    else if(date==="6月31日"){date="7月1日"}
-    else if(date==="7月32日"){date="8月1日"}
-    else if(date==="8月32日"){date="9月1日"}
-    else if(date==="9月31日"){date="10月1日"}
-    else if(date==="10月32日"){date="11月1日"}
-    else if(date==="11月31日"){date="12月1日"}
-    else if(date==="12月32日"){date="1月1日"}
+function getDay() {
+    var today = new Date();
+    var nowTime = today.getTime()+8*3600*1000;
+    today.setTime(parseInt(nowTime));
+    var oMoth = (today.getMonth() + 1).toString();
+    if (oMoth.length <= 1) oMoth = '0' + oMoth;
+    var oDay = today.getDate().toString();
+    if (oDay.length <= 1) oDay = '0' + oDay;
+    return oMoth + '月'+ oDay+'日';
+}
 
+
+app.intent('預設歡迎語句', (conv) => { 
+
+  date=getDay();
   output_array=picker.selector(date);
   Quote=output_array[0];
   reference=output_array[1];
